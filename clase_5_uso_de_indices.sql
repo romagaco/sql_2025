e9a8f61461dbcc4b8c93559af81255d6afdd5b72
roberto <roberdex26@gmail.com>
2025-07-12 17:30:38 -0600

    12/07/2025   -1
    uso de indices en sql para el mejoramiento del rendimiento

---
+++ clase_5_uso_de_indices.sql
@@ -0,0 +1,38 @@
+use sakila;
+
+/* Index B-tree  */ 
+# peliculas por año 
+
+# Btree index
+create index idx_film_year_lenght on film(release_year, length);
+
+# consulta
+# peñicñas de ñps 2006 con duracion > 120mits
+select title, release_year, length 
+from film where release_year = 2006 
+and length between 120 and 180;
+
+/* 2 index tiopo HASh */ 
+create index id_customer on customer (email);
+/* dada a la version de mysql no podemos usnar el hash pero btree nos sirve para la comparacion */
+# busqueda por email espesicico 
+# consulta de login 
+
+explain analyze
+select * from customer where email = "kathleen.adams@sakilacustomer.com";
+/* previo a la  creacion del indice el resultado en time era de 12.01*/
+/* luego del indice el resultado en time es de 0.431 */
+
+
+/* INDEX  full text */
+create fulltext index film_desc_on on film(description);
+
+/* busqueda avanzada | drama - amor - sin muerte */
+/* explain analyze  */
+select title, description from film where match (description) 
+against ("+drama -boring" in boolean mode);
+
+/* el signo +  se utiliza para indicar que queremos que la palabra si se contenga */ 
+
+/* el signo - se utiliza para indicar que queremos que la palabra no se contenga */ 
+
